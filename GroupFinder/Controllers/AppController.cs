ffc611bda6a361aa3d3492c4af2f48c81135113a
Rishiraj Mukherjee <raj@hitpromo.net>
2019-03-27 17:24:30 -0400

    Added code for checking if quiz already submitted, saving song genre and fixed some limks

--- GroupFinder/Controllers/AppController.cs
+++ GroupFinder/Controllers/AppController.cs
@@ -37,6 +37,10 @@ namespace GroupFinder.Controllers
             var item = model.FirstOrDefault();
             if (item != null)
             {
+                if(hasClassMateAnsweredQuestions(item.ClassMateId))
+                {
+                    return View("Matches");
+                }
                 Questions questions = new Questions();
                 questions.classmate = item;
                 questions.idealSaturdays = db.IdealSaturdays.ToList();
@@ -45,13 +49,14 @@ namespace GroupFinder.Controllers
                 questions.foods = db.Foods.ToList();
                 string weather = await getWeather();
                 ViewData["weather"] = weather;
+                ViewData["classMateName"] = item.fullname;
                 return View("Questions", questions);
 
             }
             else
 
             {
-                ViewBag.NotValidUser = item;
+                ViewBag.NotValidUser = "Please enter the correct email and password";
 
             }
 
@@ -60,13 +65,29 @@ namespace GroupFinder.Controllers
         }
 
 
-        public  async Task<string> getWeather()
+        public Boolean hasClassMateAnsweredQuestions(int classmateId)
+        {
+            var model = from r in db.ClassMateFoods
+                        where r.ClassMateId == classmateId
+               
+                        select r;
+            var item = model.FirstOrDefault();
+            if(item != null)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+
+        public async Task<string> getWeather()
         {
             var client = new OpenWeatherMapClient("68b8644b93b1b90aec47e87b59f8612d");
             var currentWeather = await client.CurrentWeather.GetByName("Tampa");
             //Console.WriteLine(currentWeather.Weather.Value);
-            return currentWeather.Temperature.Value.ToString() + " " +  currentWeather.Temperature.Unit + " outside you will see : " 
-                +  currentWeather.Weather.Value;
+            return currentWeather.Temperature.Value.ToString() + " " + currentWeather.Temperature.Unit + " outside you will see : "
+                + currentWeather.Weather.Value;
         }
 
         public ActionResult Questions()
@@ -74,19 +95,48 @@ namespace GroupFinder.Controllers
             return View();
         }
 
+
+        public ActionResult Matches()
+        {
+            return View();
+        }
+
+        public  SongGenre AddSongGenre(string songGenre, int classmateId)
+        {
+            var model = from r in db.SongGenres
+                        where r.genre == songGenre && r.ClassMateId == classmateId
+                        select r;
+            SongGenre item = model.FirstOrDefault();
+            if(item == null)
+            {
+                SongGenre songGenreDb = new SongGenre();
+                songGenreDb.genre = songGenre;
+                songGenreDb.ClassMateId = classmateId;
+                db.SongGenres.Add(songGenreDb);
+                db.SaveChanges();
+                item = songGenreDb;
+            }
+            return item;
+        }
+
+
         [HttpPost]
         public async Task<ActionResult> QuestionsSubmit(Questions questions)
         {
             ClassMateFood classMateFood = new ClassMateFood();
             classMateFood.ClassMateId = questions.classmateid;
             classMateFood.FoodId = questions.foodid;
-            ClassMateVacation classMateVacation= new ClassMateVacation();
+            ClassMateVacation classMateVacation = new ClassMateVacation();
             classMateVacation.ClassMateId = questions.classmateid;
             classMateVacation.VacationId = questions.vactionid;
             IdealSaturdayClassMate idealSaturdayClassMate = new IdealSaturdayClassMate();
             idealSaturdayClassMate.ClassMateId = questions.classmateid;
             idealSaturdayClassMate.IdealSaturdayId = questions.idealsaturdayid;
-            //SearchItem listsongs = await GetSpotifyData(questions.songorartist);
+            List<String> songGenres = await GetSpotifyGenreData(questions.songorartist);
+            foreach(string genre in songGenres)
+            {
+                SongGenre songGenre = AddSongGenre(genre, questions.classmateid);
+            }
             db.IdealSaturdayClassMates.Add(idealSaturdayClassMate);
             db.ClassMateVacations.Add(classMateVacation);
             db.ClassMateFoods.Add(classMateFood);
@@ -94,7 +144,24 @@ namespace GroupFinder.Controllers
             return View("ThankYou");
         }
 
-      
+
+        public async Task<List<String>> GetSpotifyGenreData(string searchTerm)
+        {
+            CredentialsAuth auth = new CredentialsAuth("078c7392711e4b978d6a3bd21984c93c", "8b7f7c3b96454fcd8b42193a179ca19b");
+            Token token = await auth.GetToken();
+            SpotifyWebAPI api = new SpotifyWebAPI() { TokenType = token.TokenType, AccessToken = token.AccessToken };
+            SearchItem item = api.SearchItems(searchTerm, SearchType.All, 10, 0, "US");
+            List<FullArtist> artists = item.Artists.Items.ToList();
+            List<String> genreList = new List<String>();
+            foreach (FullArtist artist in artists)
+            {
+                foreach (string genre in artist.Genres)
+                {
+                    genreList.Add(genre);
+                }
+            }
+            return genreList;
+        }
 
 
 
@@ -103,10 +170,10 @@ namespace GroupFinder.Controllers
             CredentialsAuth auth = new CredentialsAuth("078c7392711e4b978d6a3bd21984c93c", "8b7f7c3b96454fcd8b42193a179ca19b");
             Token token = await auth.GetToken();
             SpotifyWebAPI api = new SpotifyWebAPI() { TokenType = token.TokenType, AccessToken = token.AccessToken };
-            SearchItem item =  api.SearchItems(searchTerm, SearchType.All, 10, 0, "US");
+            SearchItem item = api.SearchItems(searchTerm, SearchType.All, 10, 0, "US");
             List<FullArtist> artists = item.Artists.Items.ToList();
             List<AjaxArtist> ajaxArtists = new List<AjaxArtist>();
-            foreach(FullArtist artist in artists)
+            foreach (FullArtist artist in artists)
             {
                 AjaxArtist artistajax = new AjaxArtist();
                 artistajax.Id = artist.Id;
@@ -115,5 +182,6 @@ namespace GroupFinder.Controllers
             }
             return Json(ajaxArtists, JsonRequestBehavior.AllowGet);
         }
+
     }
 }
